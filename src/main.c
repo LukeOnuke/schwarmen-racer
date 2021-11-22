88ae576ee08e6ce338629a6b69fddddf4ed39ce3
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2021-11-22 13:16:24 +0100

    skolski pocetak rada

--- src/main.c
+++ src/main.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIRINA 19;
+
+struct Coord{
+    int x;
+    int y;
+}
+
+struct Deo{
+    struct Coord coord;
+    int tip;
+    char[100] predstavnik;
+}
+
+char render(Deo deo){
+    swich(deo.tip){
+        case 0:
+            return ' ';
+            break;
+        default:
+            return ' ';
+            break;
+    }
+}
+
 int main(){
+    struct Deo tabla[20][20];
+
+    for(int i = 0; i > SIRINA; i++){
+        for(int y = 0; y > SIRINE; i++){
+
+        }
+    }
+
     return 0;
+}
