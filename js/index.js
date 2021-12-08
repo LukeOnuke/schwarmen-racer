461b09675ad9d3d9ed3c2412447520d9f8093fb7
Vuk <84809117+monkeypng@users.noreply.github.com>
2021-12-08 13:45:19 +0100

    pomeranje put

--- js/index.js
+++ js/index.js
@@ -21,6 +21,7 @@ gamefield.appendChild(car);
 // Kao main, koristi IIFE.
 (function() {
     setPos(car, gamefield.clientWidth * 0.5, gamefield.clientHeight * 0.3);
+    gamefield.style.backgroundPositionY = "0px";
     for (i = 0; i < 7; i++) {
         addEnemyCar();
     }
@@ -79,6 +80,8 @@ let intervalId = window.setInterval(function() {
                 }
             }
         }
+
+        gamefield.style.backgroundPositionY = (parseFloat(gamefield.style.backgroundPositionY) +  + velocity * deltaT) + "px";
 
         if(!isInside()) dead = true;
 
